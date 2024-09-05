13e641abb829c6fe6cb50a201b3ba72ba329c42c
Jeon <jeonpark94@gmail.com>
2024-09-04 20:35:44 -0700

    finished title page

--- Game.java
+++ Game.java
@@ -1,42 +1,83 @@
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
-
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Game {
 
+    //Variable declarations
     JFrame window;
     Container con;
-    JPanel titleNamePanel;
+    JPanel titleNamePanel, startButtonPanel;
     JLabel titleNameLabel;
-    Font titleFont = new Font("Times New Roman", Font.PLAIN, 28);
+    Font titleFont = new Font("Times New Roman", Font.PLAIN, 60);
+    Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
+    JButton startButton;
+
     public static void main(String []args) {
+
+    try {
+            // Set cross-platform Java L&F (also called "Metal")
+        UIManager.setLookAndFeel(
+            UIManager.getCrossPlatformLookAndFeelClassName());
+    } 
+    catch (UnsupportedLookAndFeelException e) {
+       // handle exception
+    }
+    catch (ClassNotFoundException e) {
+       // handle exception
+    }
+    catch (InstantiationException e) {
+       // handle exception
+    }
+    catch (IllegalAccessException e) {
+       // handle exception
+    }
         
         new Game();
 
     }
 
     public Game() {
+
+        // Window
         window = new JFrame();
         window.setSize(800, 600);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.getContentPane().setBackground(Color.black);
         window.setLayout(null);
-        window.setVisible(true);
         con = window.getContentPane();
 
+        // Title page
         titleNamePanel = new JPanel();
         titleNamePanel.setBounds(100, 100, 600, 150);
-        titleNamePanel.setBackground(Color.blue);
-        titleNameLabel = new JLabel("WHO ARE YOU");
+        titleNamePanel.setBackground(Color.black);
+
+        titleNameLabel = new JLabel("WHO ARE YOU?");
         titleNameLabel.setForeground(Color.white);
         titleNameLabel.setFont(titleFont);
         titleNamePanel.add(titleNameLabel);
-        con.add(titleNamePanel);
+        
+        // Start button on title page
+        startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(300, 400, 200, 100);
+        startButtonPanel.setBackground(Color.black);
 
+        startButton = new JButton("START");
+        startButton.setFont(normalFont);
+        startButton.setFocusable(false);
+        startButton.setForeground(Color.white);
+        startButton.setBackground(Color.black);
+        startButtonPanel.add(startButton);
+
+        con.add(titleNamePanel);
+        con.add(startButtonPanel);
+        
         window.setVisible(true);
     }
 };
