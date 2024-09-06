5869aef07b423c2450871574e3a1dc7d33828765
Jeon <jeonpark94@gmail.com>
2024-09-05 19:36:20 -0700

    finished tutorial and gave credit to RyiSnow in README

--- Game.java
+++ Game.java
@@ -18,14 +18,17 @@ public class Game {
     JFrame window;
     Container con;
     JPanel titleNamePanel, startButtonPanel, mainTextPanel, choiceButtonPanel, playerPanel;
-    JLabel titleNameLabel;
+    JLabel titleNameLabel, hpLabel, hpLabelNumber, weaponLabel, weaponLabelName;
     Font titleFont = new Font("Times New Roman", Font.PLAIN, 60);
-    Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
+    Font normalFont = new Font("Times New Roman", Font.PLAIN, 28);
     Font buttonFont = new Font("Times New Roman", Font.PLAIN, 22);
     JButton startButton, choice1, choice2, choice3, choice4;
     JTextArea mainTextArea;
+    int playerHP, monsterHP, silverRing;
+    String weapon, position;
 
     TitleScreenHandler tsHandler = new TitleScreenHandler();
+    ChoiceHandler choiceHandler = new ChoiceHandler();
 
     public static void main(String []args) {
 
@@ -119,40 +122,337 @@ public class Game {
         choiceButtonPanel.setBackground(Color.black);
         choiceButtonPanel.setLayout(new GridLayout(4, 1));
 
-        choice1 = choiceButton("Choice 1");
-        choice2 = choiceButton("Choice 2");
-        choice3 = choiceButton("Choice 3");
-        choice4 = choiceButton("Choice 4");
+        choice1 = choiceButton("Choice 1", choiceButtonPanel);
+        choice2 = choiceButton("Choice 2", choiceButtonPanel);
+        choice3 = choiceButton("Choice 3", choiceButtonPanel);
+        choice4 = choiceButton("Choice 4", choiceButtonPanel);
 
-        choiceButtonPanel.add(choice1);
-        choiceButtonPanel.add(choice2);
-        choiceButtonPanel.add(choice3);
-        choiceButtonPanel.add(choice4);
+        choice1.addActionListener(choiceHandler);
+        choice2.addActionListener(choiceHandler);
+        choice3.addActionListener(choiceHandler);
+        choice4.addActionListener(choiceHandler);
+        choice1.setActionCommand("c1");
+        choice2.setActionCommand("c2");
+        choice3.setActionCommand("c3");
+        choice4.setActionCommand("c4");
+        
         con.add(choiceButtonPanel);
 
         // JPanel player panel
         playerPanel = new JPanel();
         playerPanel.setBounds(100, 15, 600, 50);
-        playerPanel.setBackground(Color.blue);
+        playerPanel.setBackground(Color.black);
         playerPanel.setLayout(new GridLayout(1, 4));
         con.add(playerPanel);
+
+        // JLabel HP label
+        hpLabel = playerNumbers("HP: ", playerPanel);
+        hpLabelNumber = playerNumbers("", playerPanel);
+        weaponLabel = playerNumbers("Weapon: ", playerPanel);
+        weaponLabelName = playerNumbers("", playerPanel);
+
+        playerSetup();
 
         window.setVisible(true);
     }
 
-    public JButton choiceButton(String buttonName) {
+    public JButton choiceButton(String buttonName_m, JPanel choiceButtonPanel_m) {
+        // Method to create choice buttons
         JButton button;
-        button = new JButton(buttonName);
+        button = new JButton(buttonName_m);
         button.setBackground(Color.black);
         button.setForeground(Color.white);
+        button.setFocusable(false);
         button.setFont(buttonFont);
+        choiceButtonPanel_m.add(button);
         return button;
     }
+
+    public JLabel playerNumbers(String text_m, JPanel playerPanel_m) {
+        // Method to create player HP and weapon labels
+        JLabel label;
+        label = new JLabel(text_m);
+        label.setFont(normalFont);
+        label.setForeground(Color.white);
+        playerPanel_m.add(label);
+        return label;
+
+    }
+
+    public void playerSetup() {
+        // Initialize player HP and weapon
+        playerHP = 15;
+        monsterHP = 20;
+        weapon = "Knife";
+        weaponLabelName.setText(weapon);
+        hpLabelNumber.setText("" + playerHP);
+
+        townGate();
+
+    }
+
+    public void townGate(){
+        position = "townGate";
+
+        mainTextArea.setText("You are at the gate of the town. \nA guard is standing at the gate of the town. \n\nWhat will you do?");
+        choice1.setText("Talk to the guard");
+        choice2.setText("Attack the guard");
+        choice3.setText("Leave");
+        choice4.setText("");
+    }
+
+    public void talkGuard(){
+        position = "talkGuard";
+
+        mainTextArea.setText("Guard: Hello stranger. \nI have never seen your face. \nI'm sorry but we cannot let you into the town.");
+        choice1.setText(">");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void attackGuard(){
+        position = "attackGuard";
+
+        mainTextArea.setText("Guard: Hey don't be stupid! \n\nThe guard fought back and hit you hard. \n(You received 3 damage)");
+        playerHP = playerHP - 3;
+        hpLabelNumber.setText("" + playerHP);
+        choice1.setText(">");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void crossRoad(){
+        position = "crossRoad";
+
+        mainTextArea.setText("You are at a crossroad. \nIf you go south, you will go back to the town.");
+        choice1.setText("Go north");
+        choice2.setText("Go east");
+        choice3.setText("Go south");
+        choice4.setText("Go west");
+    }
+
+    public void north(){
+        position = "north";
+
+        mainTextArea.setText("There is a river. \nYou drink the water and rest at the riverside. \n(You HP is recovered by 2)");
+        playerHP = playerHP + 2;
+        hpLabelNumber.setText("" + playerHP);
+        choice1.setText("Go south");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void east(){
+        position = "east";
+
+        mainTextArea.setText("You walked into a forest and found a Long Sword! \n\n(You obtained a Long Sword)");
+        weapon = "Long Sword";
+        weaponLabelName.setText(weapon);
+        choice1.setText("Go west");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void west(){
+        position = "west";
+
+        mainTextArea.setText("You encounter a goblin!");
+        choice1.setText("Fight");
+        choice2.setText("Run");
+        choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void fight(){
+        position = "fight";
+
+        mainTextArea.setText("Monster HP: " + monsterHP + "\n\nWhat do you do?");
+        choice1.setText("Attack");
+        choice2.setText("Run");
+        choice3.setText("");
+        choice4.setText("");
+    }
+    public void playerAttack(){
+        position = "playerAttack";
+
+        int playerDamage = 0;
+
+        if(weapon.equals("Knife")) {
+            playerDamage = new java.util.Random().nextInt(3);
+        }
+        else if(weapon.equals("Long Sword")){
+            playerDamage = new java.util.Random().nextInt(8);
+        }
+
+        mainTextArea.setText("You attacked the monster and dealt " + playerDamage + " damage!");
+        monsterHP = monsterHP - playerDamage;
+        choice1.setText(">");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+
+    }
+
+    public void monsterAttack(){
+        position = "monsterAttack";
+
+        int monsterDamage = 0;
+        monsterDamage = new java.util.Random().nextInt(6);
+
+        mainTextArea.setText("The goblin attacked and dealt " + monsterDamage + " damage!");
+        playerHP = playerHP - monsterDamage;
+        hpLabelNumber.setText("" + playerHP);
+
+        choice1.setText(">");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+
+    }
+
+    public void win(){
+        position = "win";
+
+        mainTextArea.setText("You defeated the goblin! \nThe monster dropped a ring! \n\n(You obatined a Silver Ring)");
+        silverRing = 1;
+
+        choice1.setText("Go east");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+
+    }
+
+    public void lose(){
+        position = "lose";
+
+        mainTextArea.setText("You are dead! \n\n<GAME OVER>");
+        choice1.setText("");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+        choice1.setVisible(false);
+        choice2.setVisible(false);
+        choice3.setVisible(false);
+        choice4.setVisible(false);
+    }
+
+    public void ending(){
+        position = "ending";
+
+        mainTextArea.setText("Guard: Oh you killed that goblin? \nThank you so much! \nWelcome to our town! \n\n<THE END>");
+        choice1.setText("");
+        choice2.setText("");
+        choice3.setText("");
+        choice4.setText("");
+        choice1.setVisible(false);
+        choice2.setVisible(false);
+        choice3.setVisible(false);
+        choice4.setVisible(false);
+    }
+
 
     public class TitleScreenHandler implements ActionListener{
         public void actionPerformed(ActionEvent event){
             
             createGameScreen();
+        }
+    }
+
+    public class ChoiceHandler implements ActionListener{
+        public void actionPerformed(ActionEvent event){
+            String yourChoice = event.getActionCommand();
+
+            switch(position) {
+                case "townGate":
+                    switch(yourChoice){
+                        case "c1":
+                            if (silverRing == 1){
+                                ending();
+                            }
+                            else{
+                                talkGuard();
+                            }
+                            break;
+                        case "c2": attackGuard(); break;
+                        case "c3": crossRoad(); break;
+                        case "c4": break;
+                    }
+                    break;
+                case "talkGuard":
+                    switch(yourChoice){
+                        case "c1": townGate(); break;
+                    }
+                    break;
+                case "attackGuard":
+                    switch(yourChoice){
+                        case "c1": townGate(); break;
+                    }
+                    break;
+                case "crossRoad":
+                    switch(yourChoice){
+                        case "c1": north(); break;
+                        case "c2": east(); break;
+                        case "c3": townGate(); break;
+                        case "c4": west(); break;
+                    }
+                    break;
+                case "north":
+                    switch(yourChoice){
+                        case "c1": crossRoad(); break;
+                    }
+                    break;
+                case "east":
+                    switch(yourChoice){
+                        case "c1": crossRoad(); break;
+                    }
+                    break;
+                case "west":
+                    switch(yourChoice){
+                        case "c1": fight(); break;
+                        case "c2": crossRoad(); break;
+                    }
+                    break;
+                case "fight":
+                    switch(yourChoice){
+                        case "c1": playerAttack(); break;
+                        case "c2": crossRoad(); break;
+                    }
+                    break;
+                case "playerAttack":
+                    switch(yourChoice){
+                        case "c1":
+                        if(monsterHP < 1) {
+                            win();
+                        }
+                        else{
+                            monsterAttack();
+                        }
+                        break;
+                    }
+                    break;
+                case "monsterAttack":
+                    switch(yourChoice){
+                        case "c1":
+                        if(playerHP < 1){
+                            lose();
+                        }
+                        else{
+                            fight();
+                        }
+                        break;
+                    }
+                    break;
+                case "win":
+                    switch(yourChoice){
+                        case "c1": crossRoad(); break;
+                    }
+                    break;
+            }
         }
     }
 };
--- README.md
+++ README.md
@@ -1,20 +1,10 @@
 # Who are you?
 
-## Work-in-progress
-
-This project is currently a work in progress and I aim to achieve everything I have setout in this README.
-
 ## Overview
 
-Welcome to **Who are you?** This is a personal project created in Java, designed as a mini text-based role-playing game (RPG). The game features a classic RPG experience where players can interact with a  storyline, explore different paths, and be rewarded with a personality type reading at the end.
+Welcome to **Who are you?** This is a personal project created in Java, designed as a mini text-based role-playing game (RPG). 
 
-This project is a part of my portfolio and represents the beginning of my journey into game development. The aim is to showcase my skills in Java programming, object-oriented design, and problem-solving through game development.
-
-## Features
-
-- **Interactive Storyline**: Immerse yourself in a captivating story with various quests and challenges.
-- **Inventory Management**: Collect and manage items, weapons, and equipment.
-- **Personality Type**: Based on your playthrough, a personality type will be designated to you at the end.
+This project is a part of my portfolio and represents the beginning of my journey into game development.
 
 ## Technologies
 
@@ -22,9 +12,11 @@ This project is a part of my portfolio and represents the beginning of my journe
 - **Development Environment**: vscode
 - **Version Control**: Git and GitHub
 
-## Getting Started
+## Credit
 
-To get started with the project, follow these steps:
+This entire project was created using a tutorial by RyiSnow
+
+https://www.youtube.com/@RyiSnow
 
 1. **Clone the Repository**:
    ```bash
